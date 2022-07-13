9779c30fd7e32d48f3ff7c4b596deb241a66236d
Divya Gaurav <104293206+DivyaGaurav21@users.noreply.github.com>
2022-07-13 11:32:12 +0530

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,26 @@
+let screen=document.getElementById("input");
+let buttons=document.getElementsByClassName("btn");
+let screenValue="";
+
+
+for (let item = 0; item < buttons.length; item++) {
+    buttons[item].addEventListener('click' , (e)=>{
+        btnText=e.target.innerText;
+        console.log(btnText);
+
+        if(btnText=='x'){
+            btnText = '*' ;
+            screenValue+=btnText;
+            screen.value = screenValue;
+        }else if(btnText == 'AC'){
+            screenValue ="";
+            screen.value=screenValue;
+        }else if(btnText== '='){
+            screen.value=eval(screenValue);
+        }else{
+            screenValue+=btnText;
+            screen.value=screenValue;
+        }
+    });
+}
+
